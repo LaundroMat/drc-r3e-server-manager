a9b20dd6f5065a8e7697be7dde7e62d16275a946
Mathieu <laundro@gmail.com>
2020-12-07 22:36:55 +0100

    Start and stop local server

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,61 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+
 
 Vue.use(Vuex)
 
+
 export default new Vuex.Store({
   state: {
+    frontEndData: {
+
+    },
+    serverData: {
+      // Data received from server
+      ProcessState: {},
+      GameSetting: {}
+    }
   },
   mutations: {
+    setServerData: function (state, serverData) {
+      state.serverData = serverData;
+    }
   },
   actions: {
+    fetchServerData: function (context) {
+      axios.get('http://localhost:8088/dedi/', {
+        // params: {
+        //   // Attachment: true
+        // }
+      }).then(function (response) {
+        console.log(response);
+        context.commit('setServerData', response.data[0]);
+      });
+    },
+
+    startServer: function (context, processId) {
+      axios.post('http://localhost:8088/dedi/start', {
+        ProcessId: processId, // Server id
+        // EnableTrackRotation: true, // Don't know what this is here for
+        // body: context.state.serverData.GameSetting
+      }).catch(function (error) {
+        console.error(JSON.stringify(error));
+      }).then(function (response) {
+        console.debug(response.data);
+        context.dispatch('fetchServerData');
+      });
+    },
+    stopServer: function (context, processId) {
+      axios.post('http://localhost:8088/dedi/stop', {
+        ProcessId: processId, // Server id
+      }).catch(function (error) {
+        console.error(error);
+      }).then(function (response) {
+        console.debug(response.data);
+        context.dispatch('fetchServerData');
+      })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-if="serverIsRunning">
+      <div>
+        Server is running!
+      </div>
+      <div
+        class=""
+        @click="$store.dispatch('stopServer', processId)"
+      >
+        Stop server
+      </div>
+    </div>
+    <div
+      v-else
+      class=""
+      @click="$store.dispatch('startServer', processId)"
+    >
+      Start server
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
+  beforeMount () {
+    this.$store.dispatch('fetchServerData')
+  },
+  computed: mapState({
+    processId: state => state.serverData.GameSetting.Id,
+    serverIsRunning: state => state.serverData.ProcessState.Running
+  })
 }
 </script>
